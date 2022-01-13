3eb47d11260f35e9a607e25bfae2b4d2042cfe73
Adrián Jakubčik <45712887+AdrianJakubcik@users.noreply.github.com>
2022-01-13 10:36:36 +0100

    Add project files.

---
+++ NavigationBug/NavigationBug/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NavigationBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void NextPage_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+    }
+}
